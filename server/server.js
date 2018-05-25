7fa8ec11ccab5d9870ba846ab19aceeea8397ac0
Thomas Corbin <30200976+ThwCorbin@users.noreply.github.com>
2018-05-25 11:33:35 -0400

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -20,14 +20,38 @@ io.on("connection", socket => {
   // not all the users connected to the server
   console.log("New user connected");
 
+  socket.emit("newMessage", {
+    from: "Admin",
+    text: "Welcome to the chat app!",
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit("newMessage", {
+    from: "Admin",
+    text: "New user joined chat.",
+    createdAt: new Date().getTime()
+  });
+
   // Custom events
+  // After a user/connection emits (from the console in this example)...
+  // socket.emit("createMessage", {from: "Tom", text: "I am the Walrus"});
+  // ...to the server, the server then emits the message plus
+  // a createdAt timestamp to all connections in realtime!!!
   socket.on("createMessage", message => {
     console.log("createMessage", message);
+
     io.emit("newMessage", {
       from: message.from,
       text: message.text,
       createdAt: new Date().getTime()
     });
+
+    // broadcast emits event to every connection Except emitter
+    // socket.broadcast.emit("newMessage", {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
   });
 
   // Built-in event
